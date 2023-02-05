68e9d062a89b18015645492f539afa46191900c0
seb papet <papet.seb@gmail.com>
2023-02-05 15:00:47 +0100

    page & form new project

--- src/Controller/CvController.php
+++ src/Controller/CvController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,6 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CvController extends AbstractController
 {
     #[Route('/cv', name: 'app_cv')]
+    // #[IsGranted('ROLE_CONTRIBUTOR')]
     public function index(): Response
     {
         return $this->render('cv/cv.html.twig', [
--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Form\ProjectType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PortfolioController extends AbstractController
 {
+
     #[Route('/portfolio', name: 'app_portfolio')]
+    // #[IsGranted('ROLE_CONTRIBUTOR')]
     public function index(): Response
     {
         return $this->render('portfolio/portfolio.html.twig', [
             '' => '',
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request,): Response
+    {
+        $project = new Project();
+
+        // Create the form, linked with $category
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // For example : persiste & flush the entity
+            // And redirect to a route that display the result
+        }
+        // Render the form (best practice)
+        return $this->renderForm('portfolio/newfolio.html.twig', [
+            'form' => $form,
         ]);
     }
 }
